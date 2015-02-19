5006b82c7755f7b3f8adf13889140c18c15e7635
Daniel Rendon <ddniel16@gmail.com>
2015-02-19 16:16:50 +0000

    cambios varios... app.js config baseUrl del servidor
    git-svn-id: http://dev.irontec.com/svn/EuskalErrezetak/trunk@45 3fd5bcaa-eea0-4477-adc1-b9b82a32e199

--- application/app/scripts/app.js
+++ application/app/scripts/app.js
@@ -1,10 +1,27 @@
 'use strict';
 
+// Ionic Starter App, v0.9.20
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+
+angular.module('EuskalErrezetak.controllers', []);
+
 angular.module('EuskalErrezetak', ['ionic', 'config', 'EuskalErrezetak.controllers', 'ngResource'])
     .config(function($stateProvider, $urlRouterProvider) {
-        
+
         $stateProvider
-        
+
+        .state('app', {
+            url: '/app',
+            abstract: true,
+            templateUrl: 'modules/menu/views/menu.html',
+            controller: 'MenuCtrl'
+        })
+
         .state('app.categories', {
             url: '/categories',
             views: {
@@ -14,7 +31,7 @@ angular.module('EuskalErrezetak', ['ionic', 'config', 'EuskalErrezetak.controlle
                 }
             }
         })
-        
+
         .state('app.category', {
             url: '/categories/:id',
             views: {
@@ -24,155 +41,42 @@ angular.module('EuskalErrezetak', ['ionic', 'config', 'EuskalErrezetak.controlle
                 }
             }
         })
-        
-        .state('app.playlists', {
-            url: '/playlists',
+
+        .state('app.recipe', {
+            url: '/recipes/:id',
+            views: {
+                'menuContent': {
+                    templateUrl: 'modules/recipes/views/recipe.html',
+                    controller: 'RecipesCtrl'
+                }
+            }
+        })
+
+        .state('app.staticPages', {
+            url: '/static-page/:id',
             views: {
               'menuContent' :{
-                templateUrl: 'templates/playlists.html',
-                controller: 'PlaylistsCtrl'
+                templateUrl: 'modules/staticPages/views/staticPage.html',
+                controller: 'StaticPagesCtrl'
               }
             }
           })
 
-        .state('app', {
-          url: '/app',
-          abstract: true,
-          templateUrl: 'templates/menu.html',
-          controller: 'AppCtrl'
-        })
-
-        .state('app.search', {
-          url: '/search',
-          views: {
-            'menuContent' :{
-              templateUrl: 'templates/search.html'
-            }
-          }
-        })
-
-        .state('app.browse', {
-          url: '/browse',
-          views: {
-            'menuContent' :{
-              templateUrl: 'templates/browse.html'
-            }
-          }
-        })
-        
-
-        .state('app.single', {
-          url: '/playlists/:playlistId',
-          views: {
-            'menuContent' :{
-              templateUrl: 'templates/playlist.html',
-              controller: 'PlaylistCtrl'
-            }
-          }
-        });
-      
       // if none of the above states are matched, use this as the fallback
       $urlRouterProvider.otherwise('/app/categories');
-        
+
     })
     .run(function($ionicPlatform) {
-    
+
         $ionicPlatform.ready(function() {
-            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-            // for form inputs)
-        
             if(window.cordova && window.cordova.plugins.Keyboard) {
                 cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
             }
-        
             if(window.StatusBar) {
-                // org.apache.cordova.statusbar required
                 StatusBar.styleDefault();
             }
-        
         });
-    
-    });
-
-/**
-// Ionic Starter App, v0.9.20
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-
-angular.module('EuskalErrezetak', ['ionic', 'config', 'EuskalErrezetak.controllers'])
-
-.run(function($ionicPlatform) {
-    
-    $ionicPlatform.ready(function() {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        
-        if(window.cordova && window.cordova.plugins.Keyboard) {
-          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        }
-        
-        if(window.StatusBar) {
-          // org.apache.cordova.statusbar required
-          StatusBar.styleDefault();
-        }
-        
-    });
-    
-})
-.config(function($stateProvider, $urlRouterProvider) {
-    
-  $stateProvider
-
-    .state('app', {
-      url: '/app',
-      abstract: true,
-      templateUrl: 'templates/menu.html',
-      controller: 'AppCtrl'
     })
-
-    .state('app.search', {
-      url: '/search',
-      views: {
-        'menuContent' :{
-          templateUrl: 'templates/search.html'
-        }
-      }
-    })
-
-    .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent' :{
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent' :{
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-    .state('app.single', {
-      url: '/playlists/:playlistId',
-      views: {
-        'menuContent' :{
-          templateUrl: 'templates/playlist.html',
-          controller: 'PlaylistCtrl'
-        }
-      }
-    });
-  
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
-  
-});
-*/
+    .constant(
+        'baseUrl', 'http://test218.irontec.com/EuskalErrezetak/'
+    );
