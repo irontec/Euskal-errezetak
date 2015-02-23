36513f7bf114c05b1cf787dabc06dad18c15dcc5
Daniel Rendon <ddniel16@gmail.com>
2015-02-23 15:15:08 +0000

    git-svn-id: http://dev.irontec.com/svn/EuskalErrezetak/trunk@55 3fd5bcaa-eea0-4477-adc1-b9b82a32e199

--- application/app/modules/menu/controller.js
+++ application/app/modules/menu/controller.js
@@ -1,13 +1,18 @@
 'use strict';
 
-angular.module('EuskalErrezetak.controllers').controller('MenuCtrl', function($scope, CategoriesMapper, StaticPagesMapper, SocialNetworksMapper) {
+angular.module('EuskalErrezetak.controllers').controller('MenuCtrl', function($scope, $location, $ionicSideMenuDelegate, CategoriesMapper, StaticPagesMapper, SocialNetworksMapper, TagsMapper) {
 
     var categories = new CategoriesMapper();
     var staticPages = new StaticPagesMapper();
     var socialNetworks = new SocialNetworksMapper();
+    var tags = new TagsMapper();
 
     categories.$get().then(function(data) {
         $scope.categories = data.categories;
+    });
+    
+    tags.$get().then(function(data) {
+        $scope.tags = data.tags;
     });
     
     staticPages.$get().then(function(data) {
@@ -21,4 +26,32 @@ angular.module('EuskalErrezetak.controllers').controller('MenuCtrl', function($s
     $scope.href = function(url, target) {
         window.open(url, target);
     }
+    
+    $scope.search = function(search) {
+        
+        if (search !== undefined) {
+            
+            var name = 'all';
+            var tag = 'all';
+            var category = 'all';
+            
+            if (search.category !== undefined) {
+                category = search.category;
+            }
+            
+            if (search.tag !== undefined) {
+                tag = search.tag;
+            }
+            
+            if (search.recipe !== undefined) {
+                name = search.recipe;
+            }
+            
+            $location.url('/app/search/' + name + '/' + tag + '/' + category);
+            $ionicSideMenuDelegate.toggleRight();
+            
+        }
+        
+    };
+    
 });
---
+++ application/app/modules/search/controller.js
@@ -0,0 +1,55 @@
+'use strict';
+
+angular.module('EuskalErrezetak.controllers').controller('SearchCtrl', function($scope, $stateParams, $location, $ionicLoading, $timeout, $ionicScrollDelegate, ErrorCall, baseUrl, RecipesMapper) {
+    
+    var recipes = new RecipesMapper();
+    $scope.urlImg = baseUrl + '/image';
+    
+    $ionicLoading.show({
+        template: 'Cargando... <i class="ion-loading-a"></i>'
+    });
+    
+    console.info($stateParams);
+    
+    recipes.$get($stateParams).then(function(data) {
+        
+        $scope.recipes = data.recipes;
+        $ionicLoading.hide();
+        
+    }).catch(function(errorCallback) {
+        $ionicLoading.hide();
+        ErrorCall.Message('Hay problemas para cargar las recetas.');
+    });
+    
+    /**
+     * Orden de recetas por nombre.
+     */
+    $scope.isReverse = false;
+    $scope.revertStatus = function() {
+        if ($scope.isReverse) {
+            $scope.isReverse = false;
+        } else {
+            $scope.isReverse = true;
+        }
+    };
+    
+    /**
+     * Mostrar buscador por nombre de recetas.
+     */
+    $scope.isSearchRecipe = false;
+    $scope.searchRecipe = function() {
+        if ($scope.isSearchRecipe) {
+            $scope.isSearchRecipe = false;
+        } else {
+            $scope.isSearchRecipe = true;
+        }
+    }
+    
+    /**
+     * Cuando se busque por nombre ir al top
+     */
+    $scope.$watch('searchCategory.name', function() {
+        $ionicScrollDelegate.scrollTop();
+    });
+
+});
