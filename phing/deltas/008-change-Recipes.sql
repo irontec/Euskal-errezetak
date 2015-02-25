a6d5d797d941064d868118e409fd866f2625a9c1
Mikel Eizagirre <mikeleiza@gmail.com>
2015-02-25 17:07:19 +0000

    Archivos sql del Phing que faltaban
    git-svn-id: http://dev.irontec.com/svn/EuskalErrezetak/trunk@69 3fd5bcaa-eea0-4477-adc1-b9b82a32e199

---
+++ phing/deltas/008-change-Recipes.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `Recipes` CHANGE `name` `name`  VARCHAR( 80 )  NOT NULL COMMENT  '[ml]';
+ALTER TABLE `Recipes` CHANGE  `name_es` `name_es`  VARCHAR( 80 )  NOT NULL;
+ALTER TABLE `Recipes` CHANGE  `name_eu` `name_eu` VARCHAR( 80 )  NOT NULL;
+ALTER TABLE `Recipes` CHANGE  `time` `time` VARCHAR( 80 )  NOT NULL COMMENT  '[ml]';
---
+++ phing/deltas/009-db-generator.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `Recipes` ADD `time_eu` varchar(80) NOT NULL  COMMENT '' AFTER `time`;
+ALTER TABLE `Recipes` ADD `time_es` varchar(80) NOT NULL  COMMENT '' AFTER `time`;
